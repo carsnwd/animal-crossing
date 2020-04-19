87a85237a574ff7095cce276747617d369af8f63
Carson <carsnwd@gmail.com>
2020-04-19 01:50:24 -0400

    add currently available

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,44 +1,59 @@
 <template>
-    <div>
-        <b-table
-            :data="animals"
-            :bordered="true"
-            :focusable="false"
-            :striped="true"
-            :sort-icon="sortIcon"
-            :checkable="true">
-            <template slot-scope="props">
-                <b-table-column field="name" label="Name" sortable>
-                    {{props.row.name}}
-                </b-table-column>
-                <b-table-column field="img" label="Image" width="100px" height="100px">
-                    <img :src="props.row.img"/>
-                </b-table-column>
-                <b-table-column field="value" label="Value" numeric sortable>
-                    {{props.row.value}}
-                </b-table-column>
-                <b-table-column field="location" label="Location" sortable>
-                    {{props.row.location}}
-                </b-table-column>
-                <b-table-column field="timeOfDay" label="Time of Day" sortable>
-                    {{props.row.timeOfDay}}
-                </b-table-column>
-                <b-table-column field="monthsAvailable" label="Months Available">
-                    {{props.row.monthsAvailable.join(' ')}}
-                </b-table-column>
-            </template>
-        </b-table>
-    </div>
+  <div>
+    <b-table
+      :data="animals"
+      :bordered="true"
+      :focusable="false"
+      :striped="true"
+      :sort-icon="sortIcon"
+      :checkable="true"
+    >
+      <template slot-scope="props">
+        <b-table-column field="name" label="Name" sortable>{{props.row.name}}</b-table-column>
+        <b-table-column field="img" label="Image" width="100px" height="100px">
+          <img :src="props.row.img" />
+        </b-table-column>
+        <b-table-column field="value" label="Value" numeric sortable>{{props.row.value}}</b-table-column>
+        <b-table-column field="location" label="Location" sortable>{{props.row.location}}</b-table-column>
+        <b-table-column field="timeOfDay" label="Time of Day" sortable>{{props.row.timeOfDay}}</b-table-column>
+        <b-table-column
+          field="monthsAvailable"
+          label="Months Available"
+        >{{props.row.monthsAvailable.join(' ')}}</b-table-column>
+        <b-table-column label="Currently Available">{{isAvailable(props.row.monthsAvailable) ? "Yes" : "No"}}</b-table-column>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['animals']
-}
+  props: ["animals"],
+  methods: {
+    isAvailable(monthsAvailable) {
+      const MONTHS = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      const currentMonth = MONTHS[new Date().getMonth()];
+      return monthsAvailable.indexOf(currentMonth) > 0;
+    }
+  }
+};
 </script>
 
 <style lang="less">
-    .is-checked {
-        opacity: 50%
-    }
+.is-checked {
+  opacity: 50%;
+}
 </style>
